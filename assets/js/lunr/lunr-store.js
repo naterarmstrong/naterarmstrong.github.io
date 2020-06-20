755ae3b05e5d5dfdd326a43dfdbd014411816f8d
Nate Armstrong <narmstrong@berkeley.edu>
2020-06-19 21:50:05 -0700

    update website

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "Understanding Floating Point Error",
+        "excerpt":"Floating point is the most commonly used number format for calculations. However, most people don’t know how error is created and propagated in floating point calculations. In this post, I go over the basics of understanding floating point rounding and error bounds. Floating Point There are many explanations of floating...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "url": "http://localhost:4000/understanding-floating-point-error/",
+        "teaser": null
+      },{
+        "title": "Floating Point Error Example",
+        "excerpt":"Last post, we talked about how to understand floating point error, but the only example we consider was the trivial case of \\(a + b + c\\). In this post, we will consider the example of summing the reciprocal squares of the natural numbers up to \\(n\\), \\[s_n = \\sum_{k=1}^n...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/floating-point-error-example/",
         "teaser": null
       }]
