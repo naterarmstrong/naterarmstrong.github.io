66915b3e09988db589420c07d8af16075e142010
Nate Armstrong <naterarmstrong@gmail.com>
2023-08-01 17:11:09 -0700

    Update website
    Wrote a new post, and also updated the theming and features.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
         "title": "Understanding Floating Point Error",
         "excerpt":"Floating point is the most commonly used number format for calculations. However, most people don’t know how error is created and propagated in floating point calculations. In this post, I go over the basics of understanding floating point rounding and error bounds. Floating Point There are many explanations of floating...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/understanding-floating-point-error/",
+        "tags": ["Math","Computer Science"],
+        "url": "/understanding-floating-point-error/",
         "teaser": null
       },{
         "title": "Floating Point Error Example",
         "excerpt":"Last post, we talked about how to understand floating point error, but the only example we consider was the trivial case of \\(a + b + c\\). In this post, we will consider the example of summing the reciprocal squares of the natural numbers up to \\(n\\), \\[s_n = \\sum_{k=1}^n...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/floating-point-error-example/",
+        "tags": ["Math","Computer Science"],
+        "url": "/floating-point-error-example/",
+        "teaser": null
+      },{
+        "title": "Guess the Largest Number Paradox",
+        "excerpt":"Many statistical paradoxes boil down to an imprecise problem statement, but this is one paradox that is truly astonishing, where we seem to get information from absolutely nothing. The Problem Consider a simple game. There are two pieces of face-down paper on the table, and there is a number written...","categories": [],
+        "tags": ["Statistics"],
+        "url": "/pick-the-largest-number/",
         "teaser": null
       }]
